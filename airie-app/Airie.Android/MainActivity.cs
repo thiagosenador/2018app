65cd8328454a01369ab2d229a7c16e2f5aee185b
Thiago Senador <thiagosenadors@gmail.com>
2018-02-01 18:56:20 -0600

    android solution

---
+++ airie-app/Airie.Android/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace Airie.Android
+{
+    [Activity(Label = "Airie.Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private TextView apiResponse;
+        private Button apiButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            apiResponse = this.FindViewById<TextView>(Resource.Id.apiResponseText);
+
+            apiButton = this.FindViewById<Button>(Resource.Id.apiButton);
+            apiButton.Click += OnApiButtonClicked;
+        }
+
+        private void OnApiButtonClicked(object sender, EventArgs e)
+        {
+            apiResponse.Text = "thiago";
+        }
+    }
+}
